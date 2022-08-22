afd81b97ed1d2bcdff8bbd6952efdb1f13f31fee
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-22 18:08:09 +0530

    Create demo.m

---
+++ Code/ROI_Extraction/demo.m
@@ -0,0 +1,18 @@
+
+   ROI_path='[Path for saving Extracted ROIs]'
+    if not(isfolder(ROI_path))
+
+       mkdir(ROI_path)
+   end
+    
+    datapath_image=["RGB Image Path"];
+    datapath_mask=["Superimposed Mask Path"];
+    fileArray = dir(strcat(datapath_image,'/*.jpg'));
+    fileArray1 = dir(strcat(datapath_mask,'/*.jpg'));
+    for idx=1:length(fileArray)
+    fileNm = strcat(datapath_image,'/',fileArray(idx).name);
+    fileNm1 = strcat(datapath_mask,'/',fileArray1(idx).name);
+    [filepath,name,ext] = fileparts(fileNm);
+    disp([fileNm,fileNm1])
+    crop_New(fileNm1,fileNm); 
+    

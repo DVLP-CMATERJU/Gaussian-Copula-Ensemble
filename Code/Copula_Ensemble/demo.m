f8164ae83909e35159897793eab650082e23e4a3
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 17:51:56 +0530

    Update and rename demo_fold.m to demo.m

---
+++ Code/Copula_Ensemble/demo.m
@@ -0,0 +1,19 @@
+
+    data_B_roi=['/Dataset_name/ROI/Test/Benign/'];  %Path of Benign ROIs from Test Set%
+    data_M_roi=['/Dataset_name/ROI/Test/Malignant/']; %Path of Malignant ROIs from Test Set%
+    data_B=['/Dataset_name/Original/Test/Benign/']; %Path of Original Benign Data from Test Set%
+    data_M=['/Dataset_name/Original/Test/Malignant/'];%Path of Original Benign Data from Test Set%
+    no_B = dir(strcat(data_B,'/*.png'));
+    no_M = dir(strcat(data_M,'/*.png'));
+    no_B_roi=dir(strcat(data_B_roi,'/*.png'));
+    no_M_roi=dir(strcat(data_M_roi,'/*.png'));
+    no_B=length(no_B);
+    no_M=length(no_M);
+    no_testdata_original=no_B+no_M;
+    no_B_roi=length(no_B_roi);
+    no_M_roi=length(no_M_roi);
+    no_testdata_roi=no_B_roi+no_M_roi;
+    predict=new_copula_roi(model1,model2,no_testdata_roi);
+    predict_o=new_copula_org(model1,model2,no_testdata_original);
+    mejority_result_copula( predict,predict_o,no_testdata_original,no_B_roi,no_B,no_M);
+    

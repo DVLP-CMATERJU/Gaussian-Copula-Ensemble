d0508642225b5a1ebd416c28060041fb7289af4b
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 18:33:03 +0530

    Update and rename count_roi_new.m to count.m

---
+++ Code/Copula_Ensemble/count.m
@@ -0,0 +1,56 @@
+datapath=['/Dataset_name/Original/Test/Benign/'];
+datapath1 = ['/Dataset_name/ROI/Test/Benign/'];
+	fileArray = dir(strcat(datapath,'/*.png'));
+	fileArray1 = dir(strcat(datapath1,'/*.png'));
+	for idx=1:length(fileArray)
+	    fileNm = strcat(datapath,'/',fileArray(idx).name);
+	    [filepath,name,ext] = fileparts(fileNm);
+        count_roi=0;
+	    for id=1:length(fileArray1)
+	      fileNm1 = strcat(datapath1,'/',fileArray1(id).name);
+	      img = imread(fileNm1);
+	      [filepath,name1,ext] = fileparts(fileNm1);
+          
+	      part = strsplit(name1,'.');
+	      Nm = part{1,1};
+	      if (isequal(Nm,name)==1)
+              count_roi=count_roi+1;
+             
+		 
+	      end
+        end
+        gt(idx,1)=idx;
+        gt(idx,2)=count_roi;
+    end
+    
+
+    x=length(fileArray);
+datapath=['/Dataset_name/Original/Test/Malignant/'];
+datapath1 = ['/Dataset_name/ROI/Test/Malignant/'];
+	fileArray = dir(strcat(datapath,'/*.png'));
+	fileArray1 = dir(strcat(datapath1,'/*.png'));
+	for idx=1:length(fileArray)
+	    fileNm = strcat(datapath,'/',fileArray(idx).name);
+	    [filepath,name,ext] = fileparts(fileNm);
+        count_roi=0;
+	    for id=1:length(fileArray1)
+	      fileNm1 = strcat(datapath1,'/',fileArray1(id).name);
+	      img = imread(fileNm1);
+	      [filepath,name1,ext] = fileparts(fileNm1);
+          
+	      part = strsplit(name1,'.');
+	      Nm = part{1,1};
+	      if (isequal(Nm,name)==1)
+              count_roi=count_roi+1;
+            
+		 
+	      end
+        end
+        gt(idx+x,1)=idx+x;
+        gt(idx+x,2)=count_roi;
+    end
+    
+    
+    
+ save('./count.mat','gt');
+    

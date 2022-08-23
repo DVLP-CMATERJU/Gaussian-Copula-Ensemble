7de0d3c3a7ec12735e26ea250b019129e21c3597
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 17:45:56 +0530

    Update demo.m

--- Code/ROI_Extraction/demo.m
+++ Code/ROI_Extraction/demo.m
@@ -1,13 +1,16 @@
 
-   
-   ROI_path='/Dataset_Name/ROI/''[Path for saving Extracted ROIs]'
+   folder=["Train","Test","Validation"];
+   class=["Benign","malignant"];
+   for i=1:3
+   for j=1:2
+   ROI_path=['/Dataset_Name/ROI/',folder(i),'/',class(j),'/'] %Path for saving Extracted ROIs%
     if not(isfolder(ROI_path))
 
        mkdir(ROI_path)
    end
     
-    datapath_image=["RGB Image Path"];
-    datapath_mask=["Superimposed Mask Path"];
+    datapath_image=['/Dataset_Name/Original/',folder(i),'/',class(j),'/']; %["RGB Image Path"];%
+    datapath_mask=['/Dataset_Name/Mask/',folder(i),'/',class(j),'/']; %["Superimposed Mask Path"]%
     fileArray = dir(strcat(datapath_image,'/*.jpg'));
     fileArray1 = dir(strcat(datapath_mask,'/*.png'));
     for idx=1:length(fileArray)
@@ -17,4 +20,5 @@
       disp([fileNm,fileNm1])
       crop_New(fileNm1,fileNm,ROI_path); 
     end
-    
+    end
+    end

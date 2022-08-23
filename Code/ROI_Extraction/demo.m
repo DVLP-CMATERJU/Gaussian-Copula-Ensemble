cb387a936fca78dde2423a157096d4e1d23ac34e
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 13:08:25 +0530

    Update demo.m

--- Code/ROI_Extraction/demo.m
+++ Code/ROI_Extraction/demo.m
@@ -8,11 +8,12 @@
     datapath_image=["RGB Image Path"];
     datapath_mask=["Superimposed Mask Path"];
     fileArray = dir(strcat(datapath_image,'/*.jpg'));
-    fileArray1 = dir(strcat(datapath_mask,'/*.jpg'));
+    fileArray1 = dir(strcat(datapath_mask,'/*.png'));
     for idx=1:length(fileArray)
-    fileNm = strcat(datapath_image,'/',fileArray(idx).name);
-    fileNm1 = strcat(datapath_mask,'/',fileArray1(idx).name);
-    [filepath,name,ext] = fileparts(fileNm);
-    disp([fileNm,fileNm1])
-    crop_New(fileNm1,fileNm,ROI_path); 
+      fileNm = strcat(datapath_image,'/',fileArray(idx).name);
+      fileNm1 = strcat(datapath_mask,'/',fileArray1(idx).name);
+      [filepath,name,ext] = fileparts(fileNm);
+      disp([fileNm,fileNm1])
+      crop_New(fileNm1,fileNm,ROI_path); 
+    end
     

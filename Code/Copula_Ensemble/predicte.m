8a4c97305dd527ff8ca0aa671ff117c3843fcee6
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 18:28:04 +0530

    Update predicte.m

--- Code/Copula_Ensemble/predicte.m
+++ Code/Copula_Ensemble/predicte.m
@@ -1,8 +1,8 @@
-function [ accuracy ] =predicte( result,gt,predict_o,no_testdata_original,no_B,no_M)
-%predict_o
+function [ accuracy ] =predicte( result,gt,predict_o,no_testdata_original,no_B,no_M,p)
+
 for i=1:no_testdata_original
     accuracy=(result(i,3)/gt(i,2))*100;
-    if(accuracy>=9)
+    if(accuracy>=p)
         prediction(i)=2;
     elseif (gt(i,2)==0)
         prediction(i)=predict_o(i);
@@ -10,18 +10,9 @@ for i=1:no_testdata_original
     else  prediction(i)=1;
     end
 end
-% for i=14:25
-%     accuracy=(result(i,3)/gt(i,2))*100;
-%     if(accuracy>=20)
-%         prediction(i)=2;
-%     elseif (gt(i,2)==0)
-%         prediction(i)=predict_o(i);
-%     else prediction(i)=1;
-%     end
-% end
+
 prediction=prediction';
-%prediction
-%save('/media/aritra/STORAGE1/soumyajyotiFNAC/Project_ROI/ROI_breakhis/majority_voting/copula_result/prediction/inception.mat','prediction');
+
 for i=1:no_B
     original(i)=1;
 end
@@ -29,14 +20,16 @@ for i=no_B+1:no_testdata_original
     original(i)=2;
 end
 original=original';
-%prediction
+
 correct=0;
 for i=1:no_testdata_original
     if(prediction(i,1)==original(i,1))
         correct=correct+1;
     end
 end
-%correct
+
 accuracy=(correct/no_testdata_original)*100
-%dlmwrite('/media/aritra/STORAGE1/soumyajyotiFNAC/Project_ROI/ROI_breakhis/majority_voting/accuracy.txt',accuracy);
+[c_matrix,Result]= confusion.getMatrix(original,prediction)
+[X,Y,T,AUC]=perfcurve(original,prediction,2)
+
 end        

0135f97dec74d040140b91c1a09126c3f927e572
SOUMYAJYOTI DEY <31403584+Jyoti93@users.noreply.github.com>
2022-08-23 18:10:16 +0530

    Update Probability_cal.py

--- Code/Probability_cal.py
+++ Code/Probability_cal.py
@@ -30,6 +30,7 @@ c1=[]
 c2=[]
 model_name='resnet18' ## CNN Model Name ##
 d='Original' ## For ROI, Write 'ROI' ##
+k='' ## "Train"/ "Test"/ "Validation" ##
 
 # Paths
 
@@ -73,6 +74,6 @@ for i,batch in enumerate(data_loader):
 c1_df=pd.DataFrame(c1)
 c2_df=pd.DataFrame(c2)
 
-c1_df.to_csv(save_path+model_name+'_'+d+'_B.csv', index=False, header=False)
-c2_df.to_csv(save_path+model_name+'_'+d+'_M.csv', index=False, header=False)
+c1_df.to_csv(save_path+model_name+'_'+d+'_'+k+'_B.csv', index=False, header=False)
+c2_df.to_csv(save_path+model_name+'_'+d+'_'+k+'_M.csv', index=False, header=False)
 
